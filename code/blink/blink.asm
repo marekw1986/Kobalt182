a03db30e04ef7af41dbdc0c4a862c2982f99887e
marekw1986 <marekw1986@gmail.com>
2025-08-28 14:38:47 +0200

    Blink update

--- code/blink/blink.asm
+++ code/blink/blink.asm
@@ -5,6 +5,9 @@ START:
         ; ----------------------------
         ; Init PIO Port C
         ; ----------------------------
+		; Set Port C to GPIO (parallel)
+        MVI   A, 80H      ; SCR bit 7 = 1, other bits = 0
+        OUT   SCR         ; Use definitions.asm to define SCR
         ; Configure only PC5 as output (others remain inputs)
         MVI   A, 20H          ; bit5 = 1 → PC5 output
         OUT   OMCR            ; use label from definitions.asm
