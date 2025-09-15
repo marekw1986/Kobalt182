73fa88965d4c176fb04363c4cfe14ac67d30479b
Marek Wiecek <marekw1986@gmail.com>
2025-09-15 18:58:04 +0200

    blink_count added

---
+++ code/blink_count/blink_count_kobalt.asm
@@ -0,0 +1,73 @@
+        include "../common/definitions.asm"
+
+        ORG   0000H
+START:  
+        DI
+
+        ; ----------------------------
+        ; MMU / RAM mapping
+        ; ----------------------------
+        ; --- Program ROM window (0x0000–0x7FFF) ---
+        MVI   A, 07H        ; upper block = 0x07 (0x7000–0x7FFF)
+        OUT   ROMBR         ; I/O E8h
+
+        ; --- Program RAM window (0x8000–0xFFFF) ---
+        MVI   A, 08H        ; lower block = 0x08 (0x8000)
+        OUT   RAMLBR        ; I/O E7h
+        MVI   A, 0FH        ; upper block = 0x0F (0xF000–0xFFFF)
+        OUT   RAMUBR        ; I/O E6h
+
+        ; Configure Wait State Generator
+        XRA A
+        DB 0EDH, 039H, 032H     ; OUT0 32H
+        MVI   A, 99H        
+        OUT   WSG           ; I/O addr 0xD8
+
+        ; Small delay to let bus decode settle (optional)
+        NOP
+        NOP
+        
+        ; ----------------------------
+        ; Init PIO Port A
+        ; ----------------------------
+		; Set Port B to GPIO (parallel)
+        MVI   A, 60H      ; SCR bit 7 = 0, bit 6 = 1, bit 5 = 1, other bits = 0
+        OUT   SCR         ; Use definitions.asm to define SCR
+        ; Configure all lines as outputs
+        XRA A
+        OUT   PADIR            ; use label from definitions.asm
+        ; Ensure all PA lines low
+        XRA A
+        OUT   PADATA            ; use label from definitions.asm
+        
+        MVI   A, 1FH                ; PB0..Pb4 inputs, PB5..PB7 outputs
+        OUT   PBDIR
+        XRA A
+        OUT   PBDATA
+        
+        ; Setup stack to top of mapped RAM
+        LXI   H, 8200H       ; Stack top
+        SPHL
+        
+        NOP
+        NOP
+        NOP
+        
+        MVI H, 0
+
+; ----------------------------
+; Blink loop on PA
+; ----------------------------
+LOOP:
+        INR H
+        MOV A, H
+        OUT PADATA
+        
+        MVI   C, 255
+        CALL DELAY
+
+        JMP   LOOP
+        
+        include "../common/utils.asm"
+
+        END
---
+++ code/blink_count/blink_count_kobalt_z180.asm
@@ -0,0 +1,73 @@
+        include "../common/definitions.asm"
+
+        ORG   0000H
+START:  
+        DI
+
+        ; ----------------------------
+        ; MMU / RAM mapping
+        ; ----------------------------
+        ; --- Program ROM window (0x0000–0x7FFF) ---
+        LD   A, 07H        ; upper block = 0x07 (0x7000–0x7FFF)
+        OUT   (ROMBR), A         ; I/O E8h
+
+        ; --- Program RAM window (0x8000–0xFFFF) ---
+        LD   A, 08H        ; lower block = 0x08 (0x8000)
+        OUT   (RAMLBR), A        ; I/O E7h
+        LD   A, 0FH        ; upper block = 0x0F (0xF000–0xFFFF)
+        OUT   (RAMUBR), A        ; I/O E6h
+
+        ; Configure Wait State Generator
+        LD   A, 00H
+        OUT0 (DCNTL), A     ; DB 0EDH, 039H, 032H
+        LD   A, 99H        
+        OUT   (WSG), A           ; I/O addr 0xD8
+
+        ; Small delay to let bus decode settle (optional)
+        NOP
+        NOP
+        
+        ; ----------------------------
+        ; Init PIO Port A
+        ; ----------------------------
+		; Set Port B to GPIO (parallel)
+        LD   A, 60H      ; SCR bit 7 = 0, bit 6 = 1, bit 5 = 1, other bits = 0
+        OUT   (SCR), A         ; Use definitions.asm to define SCR
+        ; Configure all lines as outputs
+        LD   A, 00H
+        OUT   (PADIR), A            ; use label from definitions.asm
+        ; Ensure all PA lines low
+        XOR A
+        OUT   (PADATA), A            ; use label from definitions.asm
+        
+        LD   A, 1FH                ; PB0..Pb4 inputs, PB5..PB7 outputs
+        OUT   (PBDIR), A
+        XOR A
+        OUT   (PBDATA), A
+        
+        ; Setup stack to top of mapped RAM
+        LD   HL, 8200H       ; Stack top
+        LD   SP, HL
+        
+        NOP
+        NOP
+        NOP
+        
+        LD H, 0
+
+; ----------------------------
+; Blink loop on PA
+; ----------------------------
+LOOP:
+        INC H
+        LD A, H
+        OUT   (PADATA), A
+        
+        LD   C, 255
+        CALL DELAY
+
+        JP   LOOP
+        
+        include "../common/utils_z180.asm"
+
+        END
